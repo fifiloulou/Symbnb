5d72483b84809443df0d04e707982d3012a594a0
philippe <philippe.favero@gmail.com>
2020-04-18 18:32:42 +0200

    Comprendre l'Injection de Dépendances

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Ad;
+use App\Repository\AdRepository;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdController extends AbstractController
@@ -11,10 +13,12 @@ class AdController extends AbstractController
     /**
      * @Route("/ads", name="ads_index")
      */
-    public function index()
+    public function index(AdRepository $repo, SessionInterface $session)
     {
-        // Indique où récupèrer les données de la table ad
-        $repo = $this->getDoctrine()->getRepository(Ad::class);
+        dump($session);
+
+        // Indique où récupèrer les données de la table ad(les annonces)Mis en commentaire car remplacer par la ligne du dessus
+        // $repo = $this->getDoctrine()->getRepository(Ad::class);
 
         // Précise de quelle manière récupérer les données
         $ads = $repo->findAll();
