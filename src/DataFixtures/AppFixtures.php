a07799f5603f8a4cd8c2911862ee132339ea335c
philippe <philippe.favero@gmail.com>
2020-04-18 13:20:24 +0200

    Ajout de fausses données avec Faker

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Ad;
+use Faker\Factory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -10,23 +11,35 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        // Utilisation de faker pour remplir des fausses données
+        $faker = Factory::create('fR-r');
+
         // Création d'une boucle qui va répéter 30 fois l'annonce
         for($i = 1; $i <= 30; $i++) {
-            // ici on créé une nouvelle annonce ad et on ajoute le use pour dire à php où elle se trouve
+
+            // Ici on créé une nouvelle annonce ad et on ajoute le use pour dire à php où elle se trouve
             $ad = new Ad();
 
-            // Création d'une annonce
-            $ad->setTitle("Titre de l'annonce n°$i")
+            // On défini tous nos faker dans les variables
+            $title = $faker->sentence();
+            $coverImage = $faker->imageUrl(1000,350);
+            $introduction = $faker->paragraph(2);
+            $content = '<p>' . join('</p><p>', $faker->paragraphs(5)) . '</p>';
+
+
+            // Création d'une annonce en utilisant les variables pour compléter l'annonce
+            $ad->setTitle("$title")
             ->setSlug("titre-de-l-annoce-n-$i")
-            ->setCoverImage("http://placehold.it/1000x300")
-            ->setIntroduction("Bonjour à tous c'est une introduction")
-            ->setContent("<p>Je suis un contenu riche !</p>")
+            ->setCoverImage("$coverImage")
+            ->setIntroduction("$introduction")
+            ->setContent("$content")
             ->setPrice(mt_rand(40, 200))
             ->setRooms(mt_rand(1, 5));
             
             // On fait persister l'annonce dans la base de données
             $manager->persist($ad);
         }
+
         // Et on l'écrit en base de données (requête sql)
         $manager->flush();
     }
