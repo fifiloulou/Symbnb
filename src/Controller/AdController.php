de96fbe210734c1344f0523781421344bff472ad
philippe <philippe.favero@gmail.com>
2020-04-20 17:58:22 +0200

    Récupérer et sauvegarder les données du formulaire

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\Ad;
 use App\Form\AdType;
 use App\Repository\AdRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -35,11 +37,18 @@ class AdController extends AbstractController
      *
      * @return Responce
      */
-    public function create() {
+    public function create(Request $request, EntityManagerInterface $manager) {
         $ad = new Ad();
 
         // Ici j'appel le formulaire AdType.php dans Form
         $form = $this->createForm(AdType::class, $ad);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($ad);
+            $manager->flush();
+        }
 
         return $this->render('ad/new.html.twig', [
             'form' => $form->createView()
