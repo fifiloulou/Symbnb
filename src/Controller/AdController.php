133c2fd88fc9d990189fe90ffe700b25c1fb87c0
philippe <philippe.favero@gmail.com>
2020-04-20 18:20:48 +0200

    Utiliser les messages Flash pour notifier le visiteur

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -49,6 +49,11 @@ class AdController extends AbstractController
             $manager->persist($ad);
             $manager->flush();
 
+            $this->addFlash(
+                'success',
+                "L'annonce <strong>{$ad->getTitle()}</strong> a bien été enregistrée !"
+            );
+
             return$this->redirectToRoute('ads_show', [
                 'slug' =>$ad->getSlug()
             ]);
