40c26b59ba24efe46963c484a81ab8a8e959bef7
philippe <philippe.favero@gmail.com>
2020-04-23 15:21:08 +0200

    Créer un formulaire d'édition pour nos annonces

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -71,6 +71,44 @@ class AdController extends AbstractController
     }
 
     /**
+     * Permet d'afficher le formulaire d'édition
+     * 
+     * @Route("/ads/{slug}/edit", name="ads_edit")
+     *
+     * @return Responce
+     */
+    public function edit(Ad $ad, Request $request, EntityManagerInterface $manager){
+        $form = $this->createForm(AdType::class, $ad);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            foreach($ad->getImages() as $image) {
+                $image->setAd($ad);
+                $manager->persist($image);
+            }
+
+            $manager->persist($ad);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "les modifications de l'annonce <strong>{$ad->getTitle()}</strong> on bien été enregistrées !"
+            );
+
+            return$this->redirectToRoute('ads_show', [
+                'slug' =>$ad->getSlug()
+            ]);
+        }
+
+        return $this->render('ad/edit.html.twig', [
+            'form' => $form->createView(),
+            'ad' => $ad
+        ]);
+
+    }
+
+    /**
      * Permet d'afficher une seule annonce
      * 
      * @Route("/ads/{slug}", name="ads_show")
