c24cc46c78626e8ca276e4b9899336da70ac1d09
philippe <philippe.favero@gmail.com>
2020-04-23 16:49:45 +0200

    Correction d'un bug lors de la suppression d'éléments du CollectionType

--- src/Form/AdType.php
+++ src/Form/AdType.php
@@ -78,10 +78,11 @@ class AdType extends AbstractType
                 CollectionType::class,
                 [
                     'entry_type' => ImageType::class,
-                    'allow_add' => true
+                    'allow_add' => true,
+                    'allow_delete' => true
                 ]
             )
-            ;  
+        ;  
     }
 
     public function configureOptions(OptionsResolver $resolver)
