1dd752a0c91bade63303887937d22f3722c58ee0
philippe <philippe.favero@gmail.com>
2020-04-27 15:14:53 +0200

    Créer une confirmation de mot de passe durant l'inscription

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -57,6 +57,11 @@ class User implements UserInterface
     private $hash;
 
     /**
+     * @Assert\EqualTo(propertyPath="hash", message="Vous n'avez pas correctement confirmé votre mot de passe !")
+     */
+    public $passwordConfirm;
+
+    /**
      * @ORM\Column(type="string", length=255)
      * @Assert\Length(min=10, minMessage="Votre introduction doit faire au moins 10 caractères")
      */
--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -23,6 +23,7 @@ class RegistrationType extends ApplicationType
             ->add('email', EmailType::class, $this->getConfiguration("Email", "Votre adress email ..."))
             ->add('picture', UrlType::class, $this->getConfiguration("Photo de profil", "Votre avatar ..."))
             ->add('hash', PasswordType::class, $this->getConfiguration("Mot de passe", "Choississez un bon mot de passe ! ..."))
+            ->add('passwordConfirm', PasswordType::class, $this->getConfiguration("Confirmation de mot de passe", "Veuillez confirmer votre mot de passe ..."))
             ->add('introduction', TextType::class, $this->getConfiguration("Introduction", "Présentez vous en quelques mots ..."))
             ->add('description', TextareaType::class, $this->getConfiguration("Description détaillée", "C'est le moment de vous présenter en détails !"))
         ;
