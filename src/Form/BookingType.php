18ee32a1d327edf71e0323a65d503b7de11135a2
philippe <philippe.favero@gmail.com>
2020-05-06 11:37:03 +0200

    Utiliser un calendrier Javascript pour empêcher le choix de dates indisponibles

--- src/Form/BookingType.php
+++ src/Form/BookingType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class BookingType extends ApplicationType
@@ -15,10 +16,10 @@ class BookingType extends ApplicationType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('startDate', DateType::class, $this->getConfiguration("Date d'arrivée",
-            "La date à laquelle vous comptez arriver", ["widget" => "single_text"]))
-            ->add('endDate', DateType::class, $this->getConfiguration("Date de départ",
-            "La date à laquelle vous quittez les lieux", ["widget" => "single_text"]))
+            ->add('startDate', TextType::class, $this->getConfiguration("Date d'arrivée",
+            "La date à laquelle vous comptez arriver"))
+            ->add('endDate', TextType::class, $this->getConfiguration("Date de départ",
+            "La date à laquelle vous quittez les lieux"))
             ->add('comment', TextareaType::class, $this->getconfiguration(false,
             "Si vous avez un commentaire, n'hésitez pas à en faire part !", ["required" => false]))
         ;
