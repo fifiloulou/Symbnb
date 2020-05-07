1d54c48ca59d34a6361faff8418bff2e7c93b24a
philippe <philippe.favero@gmail.com>
2020-05-07 18:14:33 +0200

    Partie 5 : Refonte du formulaire d'inscription

--- src/Form/AccountType.php
+++ src/Form/AccountType.php
@@ -6,16 +6,18 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
-class AccountType extends AbstractType
+class AccountType extends ApplicationType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstName')
-            ->add('lastName')
+            ->add('firstName', TextType::class, $this->getConfiguration("Prénom", "Votre prénom ..."))
+            ->add('lastName', TextType::class, $this->getConfiguration("Nom", "Votre nom de famille ..."))
             ->add('email')
-            ->add('picture')
+            ->add('picture', UrlType::class, $this->getConfiguration("Photo de profil", "Votre avatar ..."))
             ->add('introduction')
             ->add('description')
         ;
