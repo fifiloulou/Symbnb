50c01446645d749f3622763c71fa5be2dd9f4eb3
philippe <philippe.favero@gmail.com>
2020-05-09 18:30:06 +0200

    Solution : affichez la liste des réservations

---
+++ src/Controller/AdminBookingController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\BookingRepository;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdminBookingController extends AbstractController
+{
+    /**
+     * @Route("/admin/bookings", name="admin_booking_index")
+     */
+    public function index(BookingRepository $repo)
+    {
+        $bookings = $repo->findAll();
+
+        return $this->render('admin/booking/index.html.twig', [
+            'bookings' => $bookings,
+        ]);
+    }
+}
