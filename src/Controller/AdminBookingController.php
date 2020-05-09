07a6127be08f76f01da5e13d4a329412b19f98ef
philippe <philippe.favero@gmail.com>
2020-05-09 20:12:26 +0200

    Le problème de la validation par l'entité Booking dans 2 formulaires différents

--- src/Controller/AdminBookingController.php
+++ src/Controller/AdminBookingController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\Booking;
 use App\Form\AdminBookingType;
 use App\Repository\BookingRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -26,11 +28,27 @@ class AdminBookingController extends AbstractController
      * Permet d'éditer une réservation
      * 
      * @Route("/admin/bookings/{id}/edit", name="admin_booking_edit")
-     *
+     * 
      * @return Response
      */
-    public function edit(Booking $booking) {
-        $form =$this->createForm(AdminBookingType::class);
+    public function edit(Booking $booking, Request $request, EntityManagerInterface $manager) {        
+        $form = $this->createForm(AdminBookingType::class, $booking);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $booking->setAmount(0);
+
+            $manager->persist($booking);
+            $manager->flush();
+
+            $this->addFlash(
+                'success', 
+                "La réservation n°{$booking->getId()} a bien été modifiée" 
+            );
+
+            return $this->redirectToRoute("admin_booking_index");
+        }
 
         return $this->render('admin/booking/edit.html.twig', [
             'form' => $form->createView(),
