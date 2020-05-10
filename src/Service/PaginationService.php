0e71f23614639153c3090cdab5a62c773180243e
philippe <philippe.favero@gmail.com>
2020-05-10 15:50:26 +0200

    Intégrer l'affichage de la pagination dans le service de pagination

--- src/Service/PaginationService.php
+++ src/Service/PaginationService.php
@@ -2,15 +2,42 @@
 
 namespace App\Service;
 
+use Twig\Environment;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class PaginationService {
     private $entityClass;
     private $limit = 10;
     private $currentPage = 1;
+    private $manager;
+    private $twig;
+    private $route;
 
-    public function __construct(EntityManagerInterface $manager) {
+    public function __construct(EntityManagerInterface $manager, Environment $twig, RequestStack $request) {
+        $this->route = $request->getCurrentRequest()->attributes->get('_route');
+
         $this ->manager = $manager;
+        $this->twig = $twig;
+    }
+
+    public function setRoute($route) {
+        $this->route = $route;
+
+        return $this;
+    }
+
+    public function getRoute() {
+        return $this->route;
+    }
+
+    public function display() {
+        $this->twig->display('admin/partials/pagination.html.twig', [
+            'page' => $this->currentPage,
+            'pages' => $this->getPages(),
+            'route' => $this->route
+        ]);
     }
 
     public function getPages() {
