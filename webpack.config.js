c3fdd1b3fbda37135c0832a53f860a7087dbb333
philippe <philippe.favero@gmail.com>
2020-05-10 21:06:08 +0200

    Traiter nos fichiers Javascript avec Webpack Encore

--- webpack.config.js
+++ webpack.config.js
@@ -23,8 +23,8 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/css/app.scss')
-    //.addEntry('page1', './assets/js/page1.js')
+    .addStyleEntry('app', './assets/css/app.scss')
+    .addEntry('js/app', './assets/js/app.js')
     //.addEntry('page2', './assets/js/page2.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
