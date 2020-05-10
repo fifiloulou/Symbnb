94d4255e0adb4e42efea0bd58b33da7bb073dde8
philippe <philippe.favero@gmail.com>
2020-05-10 16:05:16 +0200

    Rendre configurable le service de pagination grâce au fichier "services.yaml"

--- src/Service/PaginationService.php
+++ src/Service/PaginationService.php
@@ -14,12 +14,23 @@ class PaginationService {
     private $manager;
     private $twig;
     private $route;
+    private $templatePath;
 
-    public function __construct(EntityManagerInterface $manager, Environment $twig, RequestStack $request) {
+    public function __construct(EntityManagerInterface $manager, Environment $twig, RequestStack $request, $templatePath) {
         $this->route = $request->getCurrentRequest()->attributes->get('_route');
-
-        $this ->manager = $manager;
+        $this->manager = $manager;
         $this->twig = $twig;
+        $this->templatePath = $templatePath;
+    }
+
+    public function setTemplatePath($templatePath) {
+        $this->templatePath = $templatePath;
+
+        return $this;
+    }
+
+    public function getTemplatePath() {
+        return $this->templatePath;
     }
 
     public function setRoute($route) {
@@ -33,7 +44,7 @@ class PaginationService {
     }
 
     public function display() {
-        $this->twig->display('admin/partials/pagination.html.twig', [
+        $this->twig->display($this->templatePath, [
             'page' => $this->currentPage,
             'pages' => $this->getPages(),
             'route' => $this->route
