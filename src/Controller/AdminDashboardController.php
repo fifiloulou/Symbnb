974745e3e8fa25dfa9fc708dcf8a57df8456699e
philippe <philippe.favero@gmail.com>
2020-05-10 17:34:31 +0200

    Nos premières requêtes DQL

--- src/Controller/AdminDashboardController.php
+++ src/Controller/AdminDashboardController.php
@@ -2,18 +2,30 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminDashboardController extends AbstractController
 {
     /**
      * @Route("/admin", name="admin_dashboard")
      */
-    public function index()
+    public function index(EntityManagerInterface $manager)
     {
+        $users = $manager->createQuery('SELECT COUNT(u) FROM App\Entity\User u')->getSingleScalarResult();
+        $ads = $manager->createQuery('SELECT COUNT(a) FROM App\Entity\Ad a')->getSingleScalarResult();
+        $bookings = $manager->createQuery('SELECT COUNT(b) FROM App\Entity\Booking b')->getSingleScalarResult();
+        $comments = $manager->createQuery('SELECT COUNT(c) FROM App\Entity\Comment c')->getSingleScalarResult();
+
+
         return $this->render('admin/dashboard/index.html.twig', [
-            'controller_name' => 'AdminDashboardController',
+            'stats' => [
+                'users' => $users,
+                'ads' => $ads,
+                'bookings' => $bookings,
+                'comments' => $comments
+            ]
         ]);
     }
 }
